f38f03c751d7e5bb88e6da66a76d224f7519b50b
Juha Tauriainen <juha@bin.fi>
2020-06-15 22:13:38 +0300

    Apply eslint rules to chart stories

--- src/containers/Chart/Chart.stories.js
+++ src/containers/Chart/Chart.stories.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { Chart } from './Chart.jsx';
+import React from "react";
+
+import { Chart } from "./Chart.jsx";
 
 export default {
-  title: 'Chart',
+  title: "Chart",
   component: Chart,
 };
 
-const xAxis = [{ categories: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'] }];
+const xAxis = [
+  {
+    categories: [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday",
+    ],
+  },
+];
 
-const series = [{ name: 'temperature', data: [10, 15, 20, 25, 15, 20, 15] }];
+const series = [{ name: "temperature", data: [10, 15, 20, 25, 15, 20, 15] }];
 
 export const ChartStory = () => <Chart xAxis={xAxis} series={series} />;
