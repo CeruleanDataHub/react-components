f1a5273259f94f2865ea36f463157b187e31f502
Juha Tauriainen <juha@bin.fi>
2020-06-16 12:38:23 +0300

    Apply eslint rules to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: 'react-components',
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: {
-                    loader: 'file-loader',
-                },
-            },
-        ],
-    },
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react'
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "react-components",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        "react-dom": {
-            commonjs: "react-dom",
-            commonjs2: "react-dom",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: {
+          loader: "file-loader",
         },
-        "styled-components": {
-            commonjs: "styled-components",
-            commonjs2: "styled-components",
-        }
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
     },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+    },
+    "styled-components": {
+      commonjs: "styled-components",
+      commonjs2: "styled-components",
+    },
+  },
 };
