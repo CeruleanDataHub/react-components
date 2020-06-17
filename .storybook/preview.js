71f036c3aff670387542964ecf2354be1bdaac77
Juha Tauriainen <juha@bin.fi>
2020-06-17 12:26:24 +0300

    Add docs addon to storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import { DocsContainer, DocsPage } from "@storybook/addon-docs/blocks";
+import { addParameters } from "@storybook/react";
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage
+  }
+});
