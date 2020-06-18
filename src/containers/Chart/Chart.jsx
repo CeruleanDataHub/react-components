0507f6a467c56e63ff7f8cb579b1cbc4a74f7ee5
Juha Tauriainen <juha@bin.fi>
2020-06-18 11:02:33 +0300

    Simplify chart config

--- src/containers/Chart/Chart.jsx
+++ src/containers/Chart/Chart.jsx
@@ -1,44 +1,28 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
+
+const chartDefaults = {
+  chart: {
+    width: 625,
+    height: 299,
+    backgroundColor: "#eeeeee",
+  },
+  title: null,
+  yAxis: {
+    title: { enabled: false },
+  },
+  legend: { enabled: false },
+  credits: { enabled: false },
+};
 
 export const Chart = ({ xAxis, series }) => {
-  // hoverData is not used
-  // eslint-disable-next-line no-unused-vars
-  const [hoverData, setHoverData] = useState(null);
-
   const chartOptions = {
     xAxis,
     series,
-    chart: {
-      width: 625,
-      height: 299,
-      backgroundColor: "#eeeeee",
-    },
-    title: null,
-    yAxis: {
-      title: { enabled: false },
-    },
-    plotOptions: {
-      series: {
-        point: {
-          events: {
-            mouseOver: ({ target }) =>
-              setHoverData({ hoverData: target.category }),
-          },
-        },
-      },
-    },
-    legend: { enabled: false },
-    credits: { enabled: false },
+    ...chartDefaults,
   };
-
-  // is this something this component should handle?
-  // I think loading message should be displayed by the component which loads the data and calls this component - juhq
-  if (!xAxis || xAxis.length === 0 || !series || series.length === 0) {
-    return <div> loading ... </div>;
-  }
 
   return <HighchartsReact highcharts={Highcharts} options={chartOptions} />;
 };
