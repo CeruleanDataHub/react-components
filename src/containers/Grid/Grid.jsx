23bb8173608e184201c39247d8094bc8f8aeacfa
Juha Tauriainen <juha@bin.fi>
2020-06-22 18:04:43 +0300

    Restructure grid component to handle children better

--- src/containers/Grid/Grid.jsx
+++ src/containers/Grid/Grid.jsx
@@ -1,2 +1,17 @@
-// https://github.com/azz/styled-css-grid
-export { Grid, Cell } from "styled-css-grid";
+import PropTypes from "prop-types";
+import React from "react";
+import { Cell, Grid as GridWrapper } from "styled-css-grid";
+
+const Grid = ({ children }) => (
+  <GridWrapper columns={children.length}>{children}</GridWrapper>
+);
+
+Grid.propTypes = {
+  children: PropTypes.node
+};
+
+Grid.defaultProps = {
+  children: ""
+};
+
+export { Grid, Cell };
