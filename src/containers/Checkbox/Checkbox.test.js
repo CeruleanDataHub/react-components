9e64f2d484b54d8e5aed6ca6c73da5caf15282c4
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-06-22 13:52:06 +0300

    Refactor checkbox tests to use enzyme

--- src/containers/Checkbox/Checkbox.test.js
+++ src/containers/Checkbox/Checkbox.test.js
@@ -1,18 +1,17 @@
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import { Checkbox } from "./Checkbox";
 
 describe("Checkbox", () => {
   it("should render Checkbox", () => {
-    const component = renderer.create(<Checkbox />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const component = shallow(<Checkbox />);
+    expect(toJson(component)).toMatchSnapshot();
   });
 
   it("should render Checkbox with checked prop", () => {
-    const component = renderer.create(<Checkbox checked />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const component = shallow(<Checkbox checked />);
+    expect(toJson(component)).toMatchSnapshot();
   });
 });
