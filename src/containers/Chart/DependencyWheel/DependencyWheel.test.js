86e20a94073d57efba6fd01b3e27676382ac9bb5
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-06-23 13:11:42 +0300

    Improve DependencyWheel tests

--- src/containers/Chart/DependencyWheel/DependencyWheel.test.js
+++ src/containers/Chart/DependencyWheel/DependencyWheel.test.js
@@ -4,9 +4,42 @@ import React from "react";
 
 import { DependencyWheel } from "./DependencyWheel";
 
+const series = [
+  {
+    keys: ["from", "to", "weight"],
+    data: [
+      ["Brazil", "Canada", 5],
+      ["Brazil", "Mexico", 1],
+      ["Canada", "Brazil", 1],
+      ["Canada", "Mexico", 5],
+      ["Mexico", "Brazil", 1],
+      ["Mexico", "Canada", 5]
+    ],
+    name: "Dependency wheel series",
+    dataLabels: {
+      color: "#333",
+      textPath: {
+        enabled: true,
+        attributes: {
+          dy: 5
+        }
+      },
+      distance: 10
+    },
+    size: "95%"
+  }
+];
+
 describe("Dependency Wheel", () => {
   it("should render chart", () => {
     const component = shallow(<DependencyWheel />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("should render chart with data and title", () => {
+    const component = shallow(
+      <DependencyWheel series={series} title="Dependency wheel" />
+    );
     expect(toJson(component)).toMatchSnapshot();
   });
 });
