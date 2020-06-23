a797211c94416c3c64bef694a6c76156c28e17b3
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-06-23 12:59:36 +0300

    Add tests for Map

---
+++ src/containers/Map/Map.test.js
@@ -0,0 +1,45 @@
+import mapDataFI from "@highcharts/map-collection/countries/fi/fi-all.geo.json";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import React from "react";
+
+import { Map } from "./Map";
+
+const series = [
+  {
+    data: [
+      ["fi-3280", 0],
+      ["fi-3272", 1],
+      ["fi-3275", 2],
+      ["fi-3281", 3],
+      ["fi-3279", 4],
+      ["fi-3276", 5],
+      ["fi-3287", 6],
+      ["fi-3286", 7],
+      ["fi-3290", 8],
+      ["fi-3291", 9],
+      ["fi-3292", 10],
+      ["fi-3293", 11],
+      ["fi-3294", 12],
+      ["fi-3295", 13],
+      ["fi-3296", 14],
+      ["fi-3288", 15],
+      ["fi-3285", 16],
+      ["fi-3289", 17]
+    ],
+    mapData: mapDataFI,
+    name: "Data"
+  }
+];
+
+describe("Map", () => {
+  it("should render map with data", () => {
+    const component = shallow(<Map series={series} />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("should render map with data and title", () => {
+    const component = shallow(<Map series={series} title="Map title" />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
