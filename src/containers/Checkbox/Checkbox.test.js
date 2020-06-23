8cc57b290132f1ab056d4ce5b7a73f3ee6373cae
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-06-23 15:40:32 +0300

    Add style tests to checkbox

--- src/containers/Checkbox/Checkbox.test.js
+++ src/containers/Checkbox/Checkbox.test.js
@@ -1,4 +1,6 @@
-import { shallow } from "enzyme";
+import "jest-styled-components";
+
+import { mount, shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 import React from "react";
 
@@ -13,5 +15,17 @@ describe("Checkbox", () => {
   it("should render Checkbox with checked prop", () => {
     const component = shallow(<Checkbox checked />);
     expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("applies default styles", () => {
+    const component = mount(<Checkbox />);
+    expect(component.find("div")).toHaveStyleRule("border", "1px solid grey");
+    expect(component.find("div")).toHaveStyleRule("background", "white");
+  });
+
+  it("changes styles according to props", () => {
+    const component = mount(<Checkbox checked />);
+    expect(component.find("div")).toHaveStyleRule("border", "1px solid black");
+    expect(component.find("div")).toHaveStyleRule("background", "black");
   });
 });
