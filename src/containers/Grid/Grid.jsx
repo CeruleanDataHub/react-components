293507779e3bce1fa32fc048ecd85d4b47d30fb6
Juha Tauriainen <juha@bin.fi>
2020-06-26 19:37:15 +0300

    Add as props to grid component

--- src/containers/Grid/Grid.jsx
+++ src/containers/Grid/Grid.jsx
@@ -2,15 +2,25 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Cell, Grid as GridWrapper } from "styled-css-grid";
 
-const Grid = ({ children }) => (
-  <GridWrapper columns={children.length}>{children}</GridWrapper>
+const Grid = ({ as, children }) => (
+  <GridWrapper columns={children.length} as={as}>
+    {children}
+  </GridWrapper>
 );
 
 Grid.propTypes = {
+  /** Extend styles, property is from styled components */
+  as: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.string,
+    PropTypes.shape({ render: PropTypes.func.isRequired })
+  ]),
+  /** React node */
   children: PropTypes.node
 };
 
 Grid.defaultProps = {
+  as: null,
   children: ""
 };
 
--- src/containers/Grid/Grid.test.js
+++ src/containers/Grid/Grid.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import styled from "styled-components";
 
 import { Cell, Grid } from "./Grid";
 
@@ -13,6 +14,20 @@ describe("Grid", () => {
   it("should render grid with cell", () => {
     const component = renderer.create(
       <Grid>
+        <Cell>Content</Cell>
+      </Grid>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should render grid with extended styles", () => {
+    const CustomGrid = styled.div`
+      margin-bottom: 0.5rem;
+    `;
+
+    const component = renderer.create(
+      <Grid as={CustomGrid}>
         <Cell>Content</Cell>
       </Grid>
     );
