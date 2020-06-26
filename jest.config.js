b2311c7a16765e1a135f82f75b7c33fb489cbd4c
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-06-26 14:10:27 +0300

    Add jest config to mock css styles

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   rootDir: "src/",
   setupFilesAfterEnv: ["<rootDir>/setupTest.js"],
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>/styleMock.js"
+  }
 };
