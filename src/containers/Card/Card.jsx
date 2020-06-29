eab485b2c8837661f99a450def12fc997b515322
Juha Tauriainen <juha@bin.fi>
2020-06-29 11:06:10 +0300

    Replace custom font definition with typography component in card

--- src/containers/Card/Card.jsx
+++ src/containers/Card/Card.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { Icon } from "../Icon/Icon";
+import { Typography } from "../Typography/Typography";
 
 const CardDash = styled.div`
   margin: "2em";
@@ -12,21 +13,19 @@ const CardDash = styled.div`
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: ${(props) => props["justify-content"] || "center"};
+  justify-content: ${props => props["justify-content"] || "center"};
   margin: 1em;
   border-radius: 0.25em;
   border: 1px solid #000000;
   background-color: #ffffff;
-  height: ${(props) => props.height || "20em"};
-  width: ${(props) => props.width || "auto"};
-  max-width: ${(props) => props["max-width"] || "auto"};
-  font-family: "Open Sans", sans-serif;
-  text-align: ${(props) => props["text-align"] || "center"};
+  height: ${props => props.height || "20em"};
+  width: ${props => props.width || "auto"};
+  max-width: ${props => props["max-width"] || "auto"};
+  text-align: ${props => props["text-align"] || "center"};
   padding: 1em;
   box-sizing: border-box;
 `;
 const CardHeader = styled.h3`
-  font-family: "Exo 2", sans-serif;
   font-weight: 400;
   text-align: center;
   text-transform: uppercase;
@@ -38,9 +37,15 @@ const CardBody = styled.div`
 export const Card = ({ title, icon, children }) => (
   <CardDash>
     <CardContainer>
-      {icon && <Icon name={icon} />}
-      {title && <CardHeader>{title}</CardHeader>}
-      <CardBody>{children}</CardBody>
+      <Typography>
+        {icon && <Icon name={icon} />}
+        {title && (
+          <CardHeader>
+            <Typography exo>{title}</Typography>
+          </CardHeader>
+        )}
+        <CardBody>{children}</CardBody>
+      </Typography>
     </CardContainer>
   </CardDash>
 );
@@ -48,11 +53,11 @@ export const Card = ({ title, icon, children }) => (
 Card.propTypes = {
   title: PropTypes.string,
   icon: Icon.propTypes.name,
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 Card.defaultProps = {
   title: null,
   icon: null,
-  children: "",
+  children: ""
 };
