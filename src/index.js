b16494d86d97b1e22f7782b20224b81411f17ee7
Juha Tauriainen <juha@bin.fi>
2020-06-30 14:36:44 +0300

    Remove application export from index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { Application } from "./containers/Application";
 export { Header } from "./containers/Header/Header";
 export { Dashboard } from "./containers/Dashboard/Dashboard";
 export { Card } from "./containers/Card/Card";
