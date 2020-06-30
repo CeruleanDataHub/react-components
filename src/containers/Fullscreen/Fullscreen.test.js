f9c037f8d81622c3f6fd691f0b739a66fbd4855f
Juha Tauriainen <juha@bin.fi>
2020-06-30 16:11:30 +0300

    Add onClick tests for fullscreen component

--- src/containers/Fullscreen/Fullscreen.test.js
+++ src/containers/Fullscreen/Fullscreen.test.js
@@ -1,3 +1,4 @@
+import { shallow } from "enzyme";
 import React from "react";
 import renderer from "react-test-renderer";
 
@@ -12,6 +13,21 @@ describe("Fullscreen", () => {
 
   it("should have onClick listener", () => {
     const component = renderer.create(<Fullscreen onClick={() => null} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should fire onClick event callback function", () => {
+    const handleClick = jest.fn();
+    const component = shallow(<Fullscreen onClick={handleClick} />);
+    expect(handleClick).not.toHaveBeenCalled();
+    component.simulate("click");
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it("should call default props onClick function when no onClick property is passed", () => {
+    const component = renderer.create(<Fullscreen />);
+    component.root.props.onClick();
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
