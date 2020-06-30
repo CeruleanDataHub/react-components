5bf8ef9f9b36bcaa8a8a8767936b89a960f967e4
Juha Tauriainen <juha@bin.fi>
2020-06-30 11:43:19 +0300

    Add tests for menu

---
+++ src/containers/Menu/Menu.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import { Menu } from "./Menu";
+
+const menuItems = [
+  {
+    name: "Home",
+    icon: "home"
+  },
+  {
+    name: "Users",
+    icon: "users"
+  },
+  {
+    name: "Devices",
+    icon: "rocket"
+  },
+  {
+    name: "Costs",
+    icon: "briefcase"
+  }
+];
+
+describe("Menu", () => {
+  it("should render", () => {
+    const component = renderer.create(<Menu />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should render items", () => {
+    const component = renderer.create(<Menu items={menuItems} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should render open menu", () => {
+    const component = renderer.create(<Menu items={menuItems} menuOpen />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
