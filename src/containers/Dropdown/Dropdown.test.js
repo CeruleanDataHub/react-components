49f9a73790af810f582968bfd825bed53a5fc631
Juha Tauriainen <juha@bin.fi>
2020-06-30 16:57:46 +0300

    Add onChange and item selection test to dropdown component

--- src/containers/Dropdown/Dropdown.test.js
+++ src/containers/Dropdown/Dropdown.test.js
@@ -1,3 +1,5 @@
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
 import React from "react";
 import renderer from "react-test-renderer";
 
@@ -19,5 +21,38 @@ describe("Dropdown", () => {
     const component = renderer.create(<Dropdown items={items} isOpen />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should have onClick listener", () => {
+    const component = renderer.create(
+      <Dropdown items={items} onClick={() => null} />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should fire onClick event callback function", () => {
+    const handleClick = jest.fn();
+    const component = mount(<Dropdown items={items} onClick={handleClick} />);
+    expect(handleClick).not.toHaveBeenCalled();
+    component.find("button").simulate("click");
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it("should call default props onClick function when no onClick property is passed", () => {
+    const component = renderer.create(<Dropdown items={items} />);
+    const tree = component.toJSON();
+    component.root.props.onClick();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should be able to select item from list", () => {
+    const wrapper = mount(<Dropdown items={items} isOpen />);
+    wrapper
+      .find("input")
+      .first()
+      .simulate("change");
+
+    expect(toJson(wrapper, { mode: "deep" })).toMatchSnapshot();
   });
 });
