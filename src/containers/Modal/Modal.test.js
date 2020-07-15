a30ee7f8f51a3f88563e1756d6d364487338ffa3
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-07-15 13:36:04 +0300

    Improve Modal tests

--- src/containers/Modal/Modal.test.js
+++ src/containers/Modal/Modal.test.js
@@ -1,4 +1,4 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import React from "react";
 import { ModalProvider } from "styled-react-modal";
@@ -7,7 +7,7 @@ import { Modal } from "./Modal";
 
 describe("Modal", () => {
   it("should render open Modal with children", () => {
-    const component = shallow(
+    const component = mount(
       <ModalProvider>
         <Modal isOpen>
           <p>Modal content</p>
@@ -18,14 +18,31 @@ describe("Modal", () => {
   });
 
   it("should call default props onBackgroundClick function when no onBackgroundClick property is passed", () => {
-    const component = shallow(
+    const component = mount(
       <ModalProvider>
         <Modal isOpen>
           <p>Modal content</p>
         </Modal>
       </ModalProvider>
     );
-    component.find("Modal").invoke("onBackgroundClick");
+    component.find("Modal").prop("onBackgroundClick")();
     expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("should fire onBackgroundClick function", () => {
+    const handleClick = jest.fn();
+    const component = mount(
+      <ModalProvider>
+        <Modal isOpen onBackgroundClick={handleClick}>
+          <p>Modal content</p>
+        </Modal>
+      </ModalProvider>
+    );
+    expect(handleClick).not.toHaveBeenCalled();
+    component
+      .find("div")
+      .at(0)
+      .simulate("click");
+    expect(handleClick).toHaveBeenCalled();
   });
 });
