ddec463c43e496010e012dfb18a58e56e2becb22
Tommi <tommi.tarkki@gmail.com>
2021-01-18 14:07:44 +0200

    Allow deep snapshot testing with Enzyme for charts

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,5 +1,15 @@
 import { configure } from "enzyme";
+import React from "react";
 import "jest-enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import Highcharts from "highcharts";
 
 configure({ adapter: new Adapter() });
+
+const SetupHighchartsForTesting = () => {
+  Highcharts.useSerialIds(true);
+};
+
+SetupHighchartsForTesting();
+
+React.useLayoutEffect = React.useEffect;
