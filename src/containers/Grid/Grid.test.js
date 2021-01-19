52cbd4a8d8284595e5cade3b4c567029905e391f
Tommi <tommi.tarkki@gmail.com>
2021-01-19 14:05:16 +0200

    Optimise Grid's tests

--- src/containers/Grid/Grid.test.js
+++ src/containers/Grid/Grid.test.js
@@ -1,61 +1,16 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import styled from "styled-components";
+import { mount } from "enzyme";
 
 import { Cell, Grid } from "./Grid";
 
 describe("Grid", () => {
-  it("should render grid", () => {
-    const component = renderer.create(<Grid />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("should render grid with cell", () => {
-    const component = renderer.create(
+  it("renders", () => {
+    const component = mount(
       <Grid>
-        <Cell>Content</Cell>
+        <Cell>Some content</Cell>
       </Grid>
     );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 
-  it("should render grid with custom columns", () => {
-    const component = renderer.create(
-      <Grid columns="2fr 1fr">
-        <Cell>Content</Cell>
-      </Grid>
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("should render grid with extended styles", () => {
-    const CustomGrid = styled.div`
-      margin-bottom: 0.5rem;
-    `;
-
-    const component = renderer.create(
-      <Grid as={CustomGrid}>
-        <Cell>Content</Cell>
-      </Grid>
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("should render when extending the styles", () => {
-    const CustomGrid = styled(Grid)`
-      grid-template-columns: 4rem auto;
-    `;
-
-    const component = renderer.create(
-      <CustomGrid>
-        <Cell>Content</Cell>
-      </CustomGrid>
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(component.render()).toMatchSnapshot();
   });
 });
