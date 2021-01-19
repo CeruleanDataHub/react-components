e447db8a6a3ab0a7855a8bd03e7d011632b2e5fa
Tommi <tommi.tarkki@gmail.com>
2021-01-19 13:28:10 +0200

    Optimise CheckboxDropdown's tests

--- src/containers/CheckboxDropdown/CheckboxDropdown.test.js
+++ src/containers/CheckboxDropdown/CheckboxDropdown.test.js
@@ -1,78 +1,50 @@
 import { mount } from "enzyme";
-import toJson from "enzyme-to-json";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import { CheckboxDropdown } from "./CheckboxDropdown";
 
 const items = [
-  { value: "1", label: "Item 1" },
-  { value: "2", label: "Item 2" }
+  { value: "some value", label: "Some item" },
+  { value: "some other value", label: "Some other item" }
 ];
 
 describe("CheckboxDropdown", () => {
-  it("should render", () => {
-    const component = renderer.create(<CheckboxDropdown />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  let component;
+  let onClickMock;
+
+  beforeEach(() => {
+    onClickMock = jest.fn();
+
+    component = mount(
+      <CheckboxDropdown
+        label="hello"
+        items={items}
+        isOpen
+        onClick={onClickMock}
+      />
+    );
   });
 
-  it("should render label", () => {
-    const component = renderer.create(
-      <CheckboxDropdown label="hello" items={items} isOpen />
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  it("renders", () => {
+    expect(component.render()).toMatchSnapshot();
   });
 
-  it("should render react node label", () => {
-    const component = renderer.create(
-      <CheckboxDropdown label={<div>hello</div>} items={items} isOpen />
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  it("does not call onClick yet", () => {
+    expect(onClickMock).not.toHaveBeenCalled();
   });
 
-  it("should render in open state", () => {
-    const component = renderer.create(
-      <CheckboxDropdown items={items} isOpen />
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("should have onClick listener", () => {
-    const component = renderer.create(
-      <CheckboxDropdown items={items} onClick={() => null} />
-    );
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("should fire onClick event callback function", () => {
-    const handleClick = jest.fn();
-    const component = mount(
-      <CheckboxDropdown items={items} onClick={handleClick} />
-    );
-    expect(handleClick).not.toHaveBeenCalled();
+  it("when clicked, callback is called", () => {
     component.find("button").simulate("click");
-    expect(handleClick).toHaveBeenCalled();
-  });
 
-  it("should call default props onClick function when no onClick property is passed", () => {
-    const component = renderer.create(<CheckboxDropdown items={items} />);
-    const tree = component.toJSON();
-    component.root.props.onClick();
-    expect(tree).toMatchSnapshot();
+    expect(onClickMock).toHaveBeenCalled();
   });
 
   it("should be able to select item from list", () => {
-    const wrapper = mount(<CheckboxDropdown items={items} isOpen />);
-    wrapper
+    component
       .find("input")
       .first()
       .simulate("change");
 
-    expect(toJson(wrapper, { mode: "deep" })).toMatchSnapshot();
+    expect(component.render()).toMatchSnapshot();
   });
 });
